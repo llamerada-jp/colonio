5299cda032167f4aea8d2dbcd24cc0b4dc4ed2b4
Yuji Ito <llamerada.jp@gmail.com>
2020-09-12 23:42:38 +0900

    update test for javascript
    Signed-off-by: Yuji Ito <llamerada.jp@gmail.com>

--- test/js/test.js
+++ test/js/test.js
@@ -2,13 +2,96 @@
 
 const URL = 'ws://localhost:8080/test';
 const TOKEN = '';
+const CHANNEL = 'ch';
+const KEY = 'key';
 
 function test() {
-  let c1 = new colonio.Colonio();
-  c1.on('log', (l) => { console.log(l) });
+  let node1;
+  let node2;
+  let ps1;
+  let ps2;
+  let timer;
 
-  return c1.connect(URL, TOKEN).then(() => {
-    return c1.disconnect();
+  return ColonioModule().then(colonio => {
+    console.log('new node1');
+    node1 = new colonio.Colonio();
+    node1.on('log', (l) => {
+      l.node = 'node1';
+      if (l.level === colonio.Colonio.LOG_LEVEL_ERROR ||
+        l.level === colonio.Colonio.LOG_LEVEL_WARN) {
+        console.error(l);
+      } else {
+        console.log(l);
+      }
+    })
+
+    console.log('new node2');
+    node2 = new colonio.Colonio();
+    node2.on('log', (l) => {
+      l.node = 'node2';
+      if (l.level === colonio.Colonio.LOG_LEVEL_ERROR ||
+        l.level === colonio.Colonio.LOG_LEVEL_WARN) {
+        console.error(l);
+      } else {
+        console.log(l);
+      }
+    })
+
+    console.log('node1.connect');
+    return node1.connect(URL, TOKEN);
+
+  }).then(() => {
+    console.log('node2.connect');
+    return node2.connect(URL, TOKEN);
+
+  }).then(() => {
+    console.log('get accessor');
+    ps1 = node1.accessPubsub2D('ps');
+    node1.setPosition(0, 0);
+
+    ps2 = node2.accessPubsub2D('ps');
+    node2.setPosition(0, 0);
+
+    timer = setInterval(() => {
+      console.log('publish data');
+      ps1.publish(KEY, 0, 0, 1.0, 'from ps1');
+      ps2.publish(KEY, 0, 0, 1.0, 'from ps2');
+    }, 1000);
+
+    return new Promise((resolve, reject) => {
+      console.log('ps1 waiting data');
+      ps1.on(CHANNEL, (data) => {
+        console.log('receive ps1', data);
+        if (data === 'from ps2') {
+          resolve();
+        } else {
+          reject();
+        }
+      });
+    });
+
+  }).then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('ps2 waiting data');
+      ps2.on(CHANNEL, (data) => {
+        console.log('receive ps2', data);
+        if (data === 'from ps1') {
+          resolve();
+        } else {
+          reject();
+        }
+      });
+    });
+
+  }).then(() => {
+    clearInterval(timer);
+
+    console.log('node1.disconnect');
+    return node1.disconnect();
+
+  }).then(() => {
+    console.log('node2.disconnect');
+    return node2.disconnect();
 
   }).then(() => {
     let result = document.getElementById('result');
