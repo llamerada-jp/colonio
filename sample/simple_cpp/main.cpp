3ad0ca3fe1d1830510228732f8391fa7c4f4611c
Yuji Ito <llamerada.jp@gmail.com>
2023-01-09 12:02:30 +0900

    fix sample
    Signed-off-by: Yuji Ito <llamerada.jp@gmail.com>

--- sample/simple_cpp/main.cpp
+++ sample/simple_cpp/main.cpp
@@ -8,11 +8,10 @@
 static const double C_EARTH = 2.0 * M_PI / (40075.0 * 1000);
 static const double R_1M    = M_PI / C_EARTH;
 
-colonio::Pubsub2D* db;
-
 int main(int argc, char* argv[]) {
   try {
-    std::unique_ptr<colonio::Colonio> node(colonio::Colonio::new_instance());
+    colonio::ColonioConfig config;
+    std::unique_ptr<colonio::Colonio> node(colonio::Colonio::new_instance(config));
     node->connect("http://coloniodev:8080/colonio/core.json", "");
 
     std::cout << "Connect success." << std::endl;
@@ -20,13 +19,13 @@ int main(int argc, char* argv[]) {
 
     node->set_position(M_PI * 139.7604131 / 180.0, M_PI * 35.6858593 / 180.0);
 
-    db = &(node->access_pubsub_2d(""));
-    db->on("me", [](colonio::Pubsub2D&, const colonio::Value& value) {
-      std::cout << value.get<std::string>() << std::endl;
+    node->spread_set_handler("me", [](colonio::Colonio&, const colonio::Colonio::SpreadRequest& request) {
+      std::cout << request.message.get<std::string>() << std::endl;
     });
 
     for (int i = 0; i < 10; i++) {
-      db->publish("me", M_PI * 139.7604131 / 180.0, M_PI * 35.6858593 / 180.0, R_1M * 100, colonio::Value("hello"));
+      node->spread_post(
+          M_PI * 139.7604131 / 180.0, M_PI * 35.6858593 / 180.0, R_1M * 100, "me", colonio::Value("hello"));
       std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
